4a27446bcc30a8254aeb5a6a9b57c58a8c1ff7de
Exequiel Mesa <emeza@gamasa.com>
2022-12-22 18:01:41 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <header>
+      <h1>Titulo</h1><nav>
+      <a href="">LINK</a>
+      <a href="">LINK</a>
+      <a href="">LINK</a>
+      </nav>
+    </header>
+    <main>
+      <h2>Home</h2>
+    </main>
+    <footer>
+      <p>Exequiel 2022</p>
+    </footer>
+
+    
+    </>
+  )
 }
 
 export default App;
